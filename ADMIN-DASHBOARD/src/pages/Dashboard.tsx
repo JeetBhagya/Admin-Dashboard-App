d03ba77f8baa36a45fb685a7c076ade797b5e7f8
Bhagyajeet Borah <borahbhagya040@gmail.com>
2023-10-17 22:43:16 +0530

    Initial Commit

---
+++ ADMIN-DASHBOARD/src/pages/Dashboard.tsx
@@ -0,0 +1,14 @@
+import AdminSidebar from "../components/AdminSidebar"
+
+function Dashboard() {
+  return (
+    <div className="adminContainer">
+      <AdminSidebar/>
+      <main className="dashboard">
+        <div className="bar">ard</div>
+      </main>
+    </div>
+  )
+}
+
+export default Dashboard
