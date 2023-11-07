cba4541373f3fd443bb5dfedc552465542af68a7
Bhagyajeet Borah <borahbhagya040@gmail.com>
2023-11-07 23:47:52 +0530

    Design Charts pages: Bar, Pie, Line

--- ADMIN-DASHBOARD/src/App.tsx
+++ ADMIN-DASHBOARD/src/App.tsx
@@ -2,6 +2,7 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import {Suspense, lazy} from "react";
 import Loader from "./components/Loader";
 
+
 const Dashboard = lazy(()=>import("./pages/Dashboard")) ;
 const Products = lazy(()=>import("./pages/Products")) ;
 const Customers = lazy(()=>import("./pages/Customers")) ;
@@ -9,6 +10,9 @@ const Transaction = lazy(()=>import("./pages/Transaction")) ;
 const NewProduct = lazy(()=>import("./pages/management/NewProduct")) ;
 const ProductManagement = lazy(()=>import("./pages/management/ProductManagement")) ;
 const TransactionManagement = lazy(()=>import("./pages/management/TransactionManagement")) ;
+const Bar = lazy(()=>import("./pages/charts/Bar")) ;
+const Pie = lazy(()=>import("./pages/charts/pie")) ;
+const Line = lazy(()=>import("./pages/charts/Line")) ;
 
 const App=()=> {
   return (
@@ -20,6 +24,9 @@ const App=()=> {
           <Route path="/admin/customers" element={<Customers/>}/>
           <Route path="/admin/transaction" element={<Transaction/>}/>
           {/*charts */}
+          <Route path="/admin/chart/bar" element={<Bar/>}/>
+          <Route path="/admin/chart/pie" element={<Pie/>}/>
+          <Route path="/admin/chart/line" element={<Line/>}/>
           {/*apps */}
           {/*management */}
           <Route path="/admin/product/new" element={<NewProduct/>}/>
